0d4b679c493748edaf4b8a5b14d0f827cbed59d6
MaksonGitHub25 <makson94021@gmail.com>
2022-06-29 21:47:47 +0300

    complete very good simple nice anim

--- alex-psych/main.js
+++ alex-psych/main.js
@@ -1,1 +1,8 @@
-import './style.css';
+const secondText = document.querySelector('.text-second');
+
+setTimeout(() => {
+    secondText.style.transition = '1000ms linear';
+    secondText.style.top = '380px';
+    secondText.style.color = 'rgba(42, 168, 195, 1)';
+    secondText.style.fontSize = '2.3rem';
+}, 1000);
